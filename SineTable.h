2bb62f11114bd79a0b2ca50f1ca424662b7e5f91
thomas royal <thomas.m.royal@gmail.com>
2020-05-23 16:49:46 -0400

    Added comments to sinetable.h

--- SineTable.h
+++ SineTable.h
@@ -22,6 +22,7 @@ class SineTable{
     
 public:
     SineTable(){
+        // generate wavetable
         for (int i = 0; i < TABLE_SIZE; i++){
             double angle =
                 (double(i)/TABLE_SIZE)*MathConstants<double>::twoPi;
@@ -31,6 +32,9 @@ public:
     
     // where angle represents radians
     double get(double angle){
+        
+        // map 0-twopi to 0-tablesize
+        // and then perform linear interpolation
         double index = TABLE_SIZE*(angle/MathConstants<double>::twoPi);
         int a = static_cast<int>(floor(index)) % TABLE_SIZE;
         int b = static_cast<int>(ceil(index)) % TABLE_SIZE;
