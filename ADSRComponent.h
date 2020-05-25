092619daafab50aa25003844e85a7234496497ee
thomas royal <thomas.m.royal@gmail.com>
2020-05-25 13:25:34 -0400

    ADSRComponent: header to impl

--- ADSRComponent.h
+++ ADSRComponent.h
@@ -15,40 +15,11 @@
 
 class ADSRComponent : public HumComponent {
 public:
-    ADSRComponent(HomunculusAudioProcessor& proc, AudioProcessorValueTreeState& ps, HumLook& lf)
-        : HumComponent(proc, ps, lf)
-    {
-        setupLabel(envALabel, "A");
-        setupLabel(envDLabel, "D");
-        setupLabel(envSLabel, "S");
-        setupLabel(envRLabel, "R");
-        
-        setupAttachedSlider(attackSlider, attackAttachment, "attack");
-        setupAttachedSlider(decaySlider, decayAttachment, "decay");
-        setupAttachedSlider(sustainSlider, sustainAttachment, "sustain");
-        setupAttachedSlider(releaseSlider, releaseAttachment, "release");
-        
-        attackSlider.setNumDecimalPlacesToDisplay(3);
-        decaySlider.setNumDecimalPlacesToDisplay(3);
-        sustainSlider.setNumDecimalPlacesToDisplay(3);
-        releaseSlider.setNumDecimalPlacesToDisplay(3);
-    }
+    ADSRComponent(HomunculusAudioProcessor& proc, AudioProcessorValueTreeState& ps, HumLook& lf);
 
-    ~ADSRComponent()
-    {}
+    ~ADSRComponent();
     
-    void resized() override {
-        envALabel.setBounds(0, 0, BOX_SIZE, BOX_SIZE);
-        envDLabel.setBounds(0, BOX_SIZE, BOX_SIZE, BOX_SIZE);
-        envSLabel.setBounds(0, BOX_SIZE*2, BOX_SIZE, BOX_SIZE);
-        envRLabel.setBounds(0, BOX_SIZE*3, BOX_SIZE, BOX_SIZE);
-        
-        attackSlider.setBounds(LABEL_SIZE*2.5, 0, BOX_SIZE, BOX_SIZE);
-        decaySlider.setBounds(LABEL_SIZE*2.5, BOX_SIZE, BOX_SIZE, BOX_SIZE);
-        sustainSlider.setBounds(LABEL_SIZE*2.5, 2*BOX_SIZE, BOX_SIZE, BOX_SIZE);
-        releaseSlider.setBounds(LABEL_SIZE*2.5, 3*BOX_SIZE, BOX_SIZE, BOX_SIZE);
-
-    }
+    void resized() override;
 
 private:
     Label envALabel;
