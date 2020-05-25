65c49d31a961f825cd131c3e710de3751c8d18c9
thomas royal <thomas.m.royal@gmail.com>
2020-05-25 13:02:43 -0400

    HumComponent: header to impl

--- HumComponent.cpp
+++ HumComponent.cpp
@@ -9,3 +9,32 @@
 */
 
 #include "HumComponent.h"
+
+
+HumComponent::HumComponent(HomunculusAudioProcessor& proc, AudioProcessorValueTreeState& ps, HumLook& lf):
+processor(proc),
+params(ps),
+lookAndFeel(lf)
+{
+    setLookAndFeel(&lf);
+}
+
+HumComponent::~HumComponent()
+{
+    setLookAndFeel(nullptr);
+}
+
+void HumComponent::setupLabel(Label& label, String labelText){
+    addAndMakeVisible(label);
+    label.setText(labelText, dontSendNotification);
+    label.setJustificationType(Justification::centred);
+}
+
+void HumComponent::setupAttachedSlider(Slider& slider, unique_ptr<SliderAttachment>& attachment, String parameterName){
+    
+    addAndMakeVisible(slider);
+    attachment.reset(new SliderAttachment(params, parameterName, slider));
+    slider.setSliderStyle(Slider::Rotary);
+    slider.setTextBoxStyle(Slider::TextBoxBelow, false, BOX_SIZE, BOX_SIZE/6);
+    slider.setNumDecimalPlacesToDisplay(2);
+}
--- HumComponent.h
+++ HumComponent.h
@@ -18,33 +18,13 @@ class HumComponent : public Component {
 public:
     typedef AudioProcessorValueTreeState::SliderAttachment SliderAttachment;
 
-    HumComponent(HomunculusAudioProcessor& proc, AudioProcessorValueTreeState& ps, HumLook& lf):
-        processor(proc),
-        params(ps),
-        lookAndFeel(lf)
-    {
-        setLookAndFeel(&lf);
-    }
+    HumComponent(HomunculusAudioProcessor& proc, AudioProcessorValueTreeState& ps, HumLook& lf);
     
-    ~HumComponent()
-    {
-        setLookAndFeel(nullptr);
-    }
+    ~HumComponent();
     
-    void setupLabel(Label& label, String labelText){
-        addAndMakeVisible(label);
-        label.setText(labelText, dontSendNotification);
-        label.setJustificationType(Justification::centred);    
-    }
+    void setupLabel(Label& label, String labelText);
     
-    void setupAttachedSlider(Slider& slider, unique_ptr<SliderAttachment>& attachment, String parameterName){
-        
-        addAndMakeVisible(slider);
-        attachment.reset(new SliderAttachment(params, parameterName, slider));
-        slider.setSliderStyle(Slider::Rotary);
-        slider.setTextBoxStyle(Slider::TextBoxBelow, false, BOX_SIZE, BOX_SIZE/6);
-        slider.setNumDecimalPlacesToDisplay(2);
-    }
+    void setupAttachedSlider(Slider& slider, unique_ptr<SliderAttachment>& attachment, String parameterName);
 
     void resized() override {}
 
